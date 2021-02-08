cf8b587cce22bc0bfbecac049c6558bf17489f49
rakibtanu <rakibtanu69@gmail.com>
2021-02-08 23:37:55 +0600

    updated website

--- javascript.js
+++ javascript.js
@@ -80,10 +80,10 @@ const food=(idMeal)=>{
       <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient4} </li>
       <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient5} </li>
       <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient6} </li>
-      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient6} </li>
-      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient6} </li>
-      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient6} </li>
-      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient6} </li>
+      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient7} </li>
+      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient8} </li>
+      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient9} </li>
+      <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient10} </li>
     </div>
   </div>
     `
